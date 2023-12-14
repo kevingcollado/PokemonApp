eefeced4682ac796328ef07f63ace852ab043ba9
Kevin Gustavo Collado <kcolladojimenez@gmail.com>
2023-12-13 22:28:18 -0600

    Versión 1 terminada

--- PokemonApp/Controllers/DetailViewController/DetailViewController.swift
+++ PokemonApp/Controllers/DetailViewController/DetailViewController.swift
@@ -14,10 +14,16 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var backgroundImageView: UIView!
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
-    @IBOutlet weak var typeLabel: UILabel!
     @IBOutlet weak var attackLabel: UILabel!
     @IBOutlet weak var defenseLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var viewImageColor: UIView!
+    @IBOutlet weak var typeLabel: UILabel!
+    @IBOutlet weak var typeViewBackgroundColor: UIView!
+    
+    
+    //Variables
+    private var cornerRadius: CGFloat = 10.0
     
     //Crear ViewModel
     var detailViewModel: DetailViewModel
@@ -35,15 +41,48 @@ class DetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         configView()
+        configureShadow()
+        configureLayout()
+        configureBrackgroundTypeView()
     }
     
     func configView(){
-        self.title = "Detail Pokemon"
+        self.title = "Vista detallada"
         nameLabel.text = detailViewModel.namePokemon.capitalized
         typeLabel.text = detailViewModel.typePokemon.capitalized
-        attackLabel.text = "Ataque: \(detailViewModel.attackPokemon.description)"
-        defenseLabel.text = "Defensa: \(detailViewModel.defensePokemon.description)"
+        attackLabel.text = "Ataque: \(detailViewModel.attackPokemon.description) p."
+        defenseLabel.text = "Defensa: \(detailViewModel.defensePokemon.description) p."
         descriptionLabel.text = detailViewModel.descriptionPokemon
         imageView.kf.setImage(with: detailViewModel.getPokemonImage(detailViewModel.imagePokemon ?? NetworkConstant.shared.defaultImageAddress))
+        viewImageColor.layer.backgroundColor = detailViewModel.assignColorForDetailVC(detailViewModel.typePokemon)[0]
+        typeViewBackgroundColor.layer.backgroundColor = detailViewModel.assignColorForDetailVC(detailViewModel.typePokemon)[1]
+    }
+    
+    private func configureShadow() {
+        // How blurred the shadow should be
+        viewImageColor.layer.shadowRadius = 10.0
+
+        // How far the shadow is offset from the cell's frame
+        viewImageColor.layer.shadowOffset = CGSize(width: 0, height: 10)
+
+        // The transparency of the shadow. Ranging from 0.0 (transparent) to 1.0 (opaque).
+        viewImageColor.layer.shadowOpacity = 0.45
+
+        // The default color is black
+        viewImageColor.layer.shadowColor = UIColor.black.cgColor
+        
+        // To avoid the shadow to be clipped to the corner radius
+        viewImageColor.layer.cornerRadius = cornerRadius
+        viewImageColor.layer.masksToBounds = false
+    }
+    
+    private func configureLayout() {
+        viewImageColor.layer.cornerRadius = cornerRadius
+        viewImageColor.layer.masksToBounds = true
+    }
+    
+    private func configureBrackgroundTypeView(){
+        typeViewBackgroundColor.layer.cornerRadius = cornerRadius
+        viewImageColor.layer.masksToBounds = true
     }
 }
--- PokemonApp/ViewModels/DetailViewModel/DetailViewModel.swift
+++ PokemonApp/ViewModels/DetailViewModel/DetailViewModel.swift
@@ -6,6 +6,8 @@
 //
 
 import Foundation
+import CoreGraphics
+import UIKit
 
 class DetailViewModel{
     var pokemonClass: PokemonClass
@@ -37,4 +39,43 @@ class DetailViewModel{
         let urlImage = URL(string: pokemonClass.imageURL) /* */
         return urlImage
     }
+    
+    //Obtener color
+    func assignColorForDetailVC(_ pokemon: String) -> [CGColor] {
+        switch pokemonClass.type {
+       case "fire":
+            return [UIColor(_colorLiteralRed: 0.9373, green: 0.1373, blue: 0.2353, alpha: 1).cgColor,
+                    UIColor(_colorLiteralRed: 0.851, green: 0.0157, blue: 0.1608, alpha: 1).cgColor]
+       case "poison":
+            return [UIColor(_colorLiteralRed: 0.4824, green: 0.1725, blue: 0.749, alpha: 1).cgColor,
+                    UIColor(_colorLiteralRed: 0.3529, green: 0.0941, blue: 0.6039, alpha: 1).cgColor]
+       case "water":
+            return [UIColor(_colorLiteralRed: 0, green: 0.7059, blue: 0.8471, alpha: 1).cgColor,
+                    UIColor(_colorLiteralRed: 0, green: 0.5882, blue: 0.7804, alpha: 1).cgColor]
+       case "electric":
+            return [UIColor(_colorLiteralRed: 1, green: 0.7176, blue: 0, alpha: 1).cgColor,
+                    UIColor(_colorLiteralRed: 1, green: 0.6353, blue: 0, alpha: 1).cgColor]
+       case "normal":
+            return [UIColor(_colorLiteralRed: 0.298, green: 0.3608, blue: 0.4078, alpha: 1).cgColor,
+                    UIColor(_colorLiteralRed: 0.2745, green: 0.2863, blue: 0.298, alpha: 1).cgColor]
+       case "ground":
+            return [UIColor(_colorLiteralRed: 0.4824, green: 0.4196, blue: 0.2627, alpha: 1).cgColor,
+                    UIColor(_colorLiteralRed: 0.4078, green: 0.3373, blue: 0.2039, alpha: 1).cgColor]
+       case "flying":
+            return [UIColor(_colorLiteralRed: 0.6784, green: 0.9098, blue: 0.9569, alpha: 1).cgColor,
+                    UIColor(_colorLiteralRed: 0.2824, green: 0.7922, blue: 0.8941, alpha: 1).cgColor]
+       case "fairy":
+            return [UIColor(_colorLiteralRed: 1, green: 0.4588, blue: 0.5608, alpha: 1).cgColor,
+                    UIColor(_colorLiteralRed: 1, green: 0.302, blue: 0.4275, alpha: 1).cgColor]
+        case "bug":
+             return [UIColor(_colorLiteralRed: 0.6549, green: 0.7882, blue: 0.3412, alpha: 1).cgColor,
+                     UIColor(_colorLiteralRed: 0.4157, green: 0.6, blue: 0.3059, alpha: 1).cgColor]
+        case "dragon":
+             return [UIColor(_colorLiteralRed: 0.0941, green: 0.3059, blue: 0.4667, alpha: 1).cgColor,
+                     UIColor(_colorLiteralRed: 0, green: 0.1882, blue: 0.2863, alpha: 1).cgColor]
+       default:
+            return [UIColor(_colorLiteralRed: 1, green: 0, blue: 0.3294, alpha: 1).cgColor,
+                    UIColor(_colorLiteralRed: 0.6196, green: 0, blue: 0.349, alpha: 1).cgColor]
+       }
+   }
 }
